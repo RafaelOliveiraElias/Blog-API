1798f19e2cd2f67d689e635c949b19fca61a112b
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-07-13 19:15:40 -0300

    feat: model postCategory req11

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    categoryId: DataTypes.INTEGER,
+    postId: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false
+  });
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
